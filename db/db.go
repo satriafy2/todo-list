c4598dce6860fb68f03d7d4d3d1025b3c779a734
satriafy2 <msatriadwi@gmail.com>
2023-01-27 01:09:18 +0700

    Use Getenv

--- db/db.go
+++ db/db.go
@@ -2,8 +2,11 @@ package db
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"todo-list/model"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -11,7 +14,26 @@ import (
 var Conn *gorm.DB
 
 func Init() {
-	dsn := "host=localhost user=postgres password=root dbname=todo_list port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta",
+		dbHost,
+		dbUser,
+		dbPassword,
+		dbName,
+		dbPort,
+	)
+
 	connection, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println("DB Connection err: ", err)
